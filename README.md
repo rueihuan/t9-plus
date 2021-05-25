de040b0dd8812452129fedd72eb8b470b63b4895
Rueihuan <rueihuan@outlook.com>
2021-05-24 23:59:30 -0400

    :white_check_mark:  Add a test for unigram

--- README.md
+++ README.md
@@ -26,18 +26,22 @@ console.log(t9.predict("6666"));
 
 ### With Unigram Language Model
 
+```sh
+yarn add unigram
+```
+
 ```ts
 const { T9Search } = require("t9-plus");
+const unigram = require("unigram");
 
 const t9 = new T9Search();
 
-const unigram = require("unigram");
 const wordsWithWeight = unigram.slice(0, 20000);
 
 const map = new Map();
 
 for (let i = 0; i < wordsWithWeight.length; i++) {
-  map.set(wordsWithWeight[i]["word"], Number(wordsWithWeight[i]["freq"]));
+  map.set(wordsWithWeight[i]["word"], wordsWithWeight[i]["freq"]);
 }
 
 t9.setDictWithWeight(map);
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,31 @@
 import { T9Search } from "./";
+const unigram = require("unigram")
 
 describe("T9 Search", () => {
   it("should predict words", () => {
-    const words = ["animal", "baseball", "capital", "elephant", "morning"];
     const t9 = new T9Search();
+
+    const words = ["animal", "baseball", "capital", "elephant", "morning"];
     t9.setDict(words);
 
     const prediction = t9.predict("6");
     expect(prediction).toEqual(["morning"]);
+  });
+
+  it("should predict words with unigram", () => {
+    const t9 = new T9Search();
+
+    const wordsWithWeight = unigram.slice(0, 3000);
+
+    const map = new Map();
+
+    for (let i = 0; i < wordsWithWeight.length; i++) {
+      map.set(wordsWithWeight[i]["word"], wordsWithWeight[i]["freq"]);
+    }
+
+    t9.setDictWithWeight(map);
+
+    const prediction = t9.predict("6666");
+    expect(prediction).toEqual(["moon"]);
   });
 });

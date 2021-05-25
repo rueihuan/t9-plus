d044e2c61c63c36f387715a0b7792f78cae24687
Rueihuan <rueihuan@outlook.com>
2021-05-24 23:25:41 -0400

    :bug:  Tweak testing file

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,25 +1,12 @@
 import { T9Search } from "./";
-const words = require("an-array-of-english-words");
 
 describe("T9 Search", () => {
   it("should predict words", () => {
     const words = ["animal", "baseball", "capital", "elephant", "morning"];
-    const wordsPrioritized = ["apple", "banana", "moon"];
     const t9 = new T9Search();
     t9.setDict(words);
-    t9.setDictPrioritized(wordsPrioritized);
 
     const prediction = t9.predict("6");
-    expect(prediction.slice(0, 2)).toEqual(["moon", "morning"]);
-  });
-
-  it("should prioritize the results from Prioritized Dict", () => {
-    const wordsPrioritized = ["apple", "banana", "moon"];
-    const t9 = new T9Search();
-    t9.setDict(words);
-    t9.setDictPrioritized(wordsPrioritized);
-
-    const prediction = t9.predict("6");
-    expect(prediction.slice(0, 1)).toEqual(["moon"]);
+    expect(prediction.slice(0, 2)).toEqual(["morning"]);
   });
 });
